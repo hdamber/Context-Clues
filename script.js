7f77099ad6c7d83669f4e975c848f820b661c4bc
IB_Student <student@InnovateBhams-MacBook-Pro-4.local>
2020-02-03 14:44:31 -0600

    progress

---
+++ script.js
@@ -0,0 +1,3 @@
+var friends = ['Baby Yoda', 'Mando', 'Luke', 'Kylo Ren', 'Chewbacca'];
+var locations = ['kitchen', 'guest bathroom', 'master bedroom', 'upstairs guest bedroom', 'coat closet', 'garage', 'backyard', 'rooftop', 'master bathroom', 'dining room', 'spaceship', 'car', 'basement', 'ball room', 'stairs', 'attic', 'downstairs guest bedroom', 'treehouse', 'tool shed', 'barn']
+var weapons = ['lightsaber', 'rubber duck', 'blender', 'rock', 'violin', 'penny', 'kitten', 'garbage can', 'furby', 'CD']
